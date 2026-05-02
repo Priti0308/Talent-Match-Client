721f4d364ca731ef638435fe49befdb39b2ae992
Priti Kadam <pritikadam0308@gmail.com>
2026-05-02 18:11:19 +0530

    Fix 405 error by setting axios global baseURL

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
+import axios from "axios";
+
+axios.defaults.baseURL = "https://talent-match-9rsc.onrender.com";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
