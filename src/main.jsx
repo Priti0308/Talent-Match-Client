55ab034d1516a560aa3e97d59d72d5d46e41c355
Priti Kadam <pritikadam0308@gmail.com>
2026-05-02 18:28:43 +0530

    Make axios baseURL dynamic based on environment

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,9 @@ import "./index.css";
 
 import axios from "axios";
 
-axios.defaults.baseURL = "https://talent-match-9rsc.onrender.com";
+axios.defaults.baseURL = import.meta.env.MODE === 'production' 
+  ? "https://talent-match-9rsc.onrender.com"
+  : "http://localhost:5000";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
